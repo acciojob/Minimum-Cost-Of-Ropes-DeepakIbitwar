639b338e09a9957768770c8df2cac00414ade988
DeepakIbitwar <127574226+DeepakIbitwar@users.noreply.github.com>
2023-07-21 13:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	let form = document.getElementById("form");
+	let ropes = document.getElementById("rope-lengths").value;
+	let result = document.getElementById("result");
+	let ropeLen = ropes.split(',');
+	ropeLen.sort((a,b)=>{return a-b});
+	// console.log(ropes, ropeLen);
+	let all = [];
+	ropeLen.forEach((e)=>{
+		let i = parseInt(e);
+		all.push(i);
+	});
+	let res = 0;
+	while(all.length>1){
+		let a = all.shift();
+		let b = all.shift();
+		res += a+b;
+		all.push(a+b);
+		all.sort((a,b)=>{return a-b});
+	}
+	// console.log(res);
+	result.innerText = res;	
 }  
